b7334a3fb644515c99b391f6628c238f7b2b5ed4
gaoxj <gaoxj@certusnet.com.cn>
2019-12-24 16:43:39 +0800

    socks5 modify

--- server/proxy/socks5.go
+++ server/proxy/socks5.go
@@ -306,7 +306,6 @@ func (s *Sock5ModeServer) Auth(c net.Conn) error {
 
 //start
 func (s *Sock5ModeServer) Start() error {
-	s.RunUDPServer()
 	return conn.NewTcpListenerAndProcess(s.task.ServerIp+":"+strconv.Itoa(s.task.Port), func(c net.Conn) {
 		if err := s.CheckFlowAndConnNum(s.task.Client); err != nil {
 			logs.Warn("client id %d, task id %d, error %s, when socks5 connection", s.task.Client.Id, s.task.Id, err.Error())
@@ -316,6 +315,7 @@ func (s *Sock5ModeServer) Start() error {
 		logs.Trace("New socks5 connection,client %d,remote address %s", s.task.Client.Id, c.RemoteAddr())
 		s.handleConn(c)
 		s.task.Client.AddConn()
+		s.RunUDPServer()
 	}, &s.listener)
 }
 
